efbe4485a7d5d52e5be9ae8228d8526a5ec0b623
Fellipe Couto <fellipe@vulpi.com.br>
2021-07-23 11:59:12 -0300

    Creating a navsider and home page with Material.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,17 +7,29 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './componentes/template/header/header.component';
 
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { FooterComponent } from './componentes/template/footer/footer.component';
+import { NavComponent } from './componentes/template/nav/nav.component';
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatListModule} from "@angular/material/list";
+import { HomeComponent } from './views/home/home.component';
+import {MatCardModule} from "@angular/material/card";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
